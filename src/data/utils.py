f1d66e5b990e4e7ce5e9d775244717fa56d2f778
Belu Ticona <ticona.belu@gmail.com>
2021-03-12 15:29:31 -0300

    [src] Adding shuffle auxiliars

--- src/data/utils.py
+++ src/data/utils.py
@@ -26,8 +26,28 @@ def make_dataset(df):
 	
 	return succesful_reactions
 
+'''Generate a non sense data set:
+Shuffle all row except by crystal score column'''
+def nonsense_dataset(df):
+	out_hold['out_crystalscore'] = df['_out_crystalscore']
+    df = df.reindex(np.random.permutation(df.index)).reset_index(drop=True)
+    df['_out_crystalscore'] = out_hold['out_crystalscore']
+   return df
 
-	
+def deep_shuffe(df):
+	# Not shuffled: raw
+	keeped_columns = df.loc[:, '_raw_model_predicted':'_prototype_heteroatomINT']
+    keeped_columns = pd.concat([df['_rxn_organic-inchikey'], keeped_columns], axis=1) 
+
+    # Isolated shuffle: all but raw
+	shuffle_rxn = pd.concat([df.loc[:, 'name':'_rxn_M_organic'], 
+                      df.loc[:, '_rxn_temperatureC_actual_bulk' : '_feat_Hacceptorcount']], 
+                                     axis = 1)
+    shuffled_rxn = shuffle_deep_df.apply(np.random.permutation).reset_index(drop=True)
+
+    shuffled_reactions = pd.concat([keeped_columns, shuffled_rxn], axis=1)
+
+    return shuffled_reactions
 	
 	
 	
