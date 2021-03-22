f68792846ba5bab09723f967abc3b29245e6b766
Belu Ticona <ticona.belu@gmail.com>
2021-03-22 13:18:50 -0300

    [Rf] utils typo error and other minor rf

--- src/data/utils.py
+++ src/data/utils.py
@@ -5,26 +5,29 @@ import pandas as pd
 
 experiment_version = 1.1
 
-GBL_inchikey = 'YEJRWHAVMIAJKC-UHFFFAOYSA-N'
+GBL_inchi_key = 'YEJRWHAVMIAJKC-UHFFFAOYSA-N'
 
-dimethyl_ammonium_inchikey = 'MXLWMIFDJCGBV-UHFFFAOYSA-N'
+dimethyl_ammonium_inchi_key = 'MXLWMIFDJCGBV-UHFFFAOYSA-N'
 
-'''Generate a non sense data set:
-Shuffle all row except by crystal score column'''
+
 def shuffle(df):
+    """Generate a non sense data set:
+    Shuffle all row except by crystal score column
+    """
     out_hold = df['_out_crystalscore']
     df = df.reindex(np.random.permutation(df.index)).reset_index(drop=True)
     df['_out_crystalscore'] = out_hold
     return df
 
+
 def deep_shuffle(df):
     # Not shuffled: raw
     keeped_columns = df.loc[:, '_raw_model_predicted':'_prototype_heteroatomINT']
-    keeped_columns = pd.concat([df['_rxn_organic-inchikey'], keeped_columns], axis=1) 
+    keeped_columns = pd.concat([df['_rxn_organic-inchikey'], keeped_columns], axis=1)
 
     # Isolated shuffle: all but raw
-    shuffle_rxn = pd.concat([df.loc[:, 'name':'_rxn_M_organic'], 
-                            df.loc[:, '_rxn_temperatureC_actual_bulk' : '_feat_Hacceptorcount']], 
+    shuffle_rxn = pd.concat([df.loc[:, 'name':'_rxn_M_organic'],
+                            df.loc[:, '_rxn_temperatureC_actual_bulk' : '_feat_Hacceptorcount']],
                             axis = 1)
     shuffled_rxn = shuffle_rxn.apply(np.random.permutation).reset_index(drop=True)
 
@@ -32,28 +35,30 @@ def deep_shuffle(df):
 
     return shuffled_reactions
 
+
 def prepare_dataset(df, data_preparation):
-    
+
     # Select data from version 1.1
     df.query('_raw_ExpVer == @experiment_version', inplace = True)
 
     # Select reactions where only GBL is used as solvent 
-    df.query('_raw_reagent_0_chemicals_0_InChIKey == @GBL_inchikey', inplace = True)
-    
+    df.query('_raw_reagent_0_chemicals_0_InChIKey == @GBL_inchi_key', inplace = True)
+
     # Remove some anomalous entries with dimethyl ammonium still listed as the organic. 
-    df.query('_raw_reagent_0_chemicals_0_InChIKey != @dimethyl_ammonium_inchikey', inplace = True) 
-    
-    ## DECISION FUERTE, POR QUÉ NO CONSIDERAR LOS QUE TENGAN 3?
-    # Collect inchikeys of solvents that had at least a succesful crystal
-    
-    succesful_inchikeys = df.query('_out_crystalscore == 4')['_rxn_organic-inchikey'].unique()
-    
-    df = (df[df['_rxn_organic-inchikey'].isin(succesful_inchikeys)])
-    
-    # If it is desired to make a non sence dataset
-    if data_preparation["shuffle_enabled"]: 
+    df.query('_raw_reagent_0_chemicals_0_InChIKey != @dimethyl_ammonium_inchi_key', inplace = True)
+
+    """DECISION FUERTE, POR QUÉ NO CONSIDERAR LOS QUE TENGAN 3?
+     Collect inches of solvents that had at least a successful crystal
+    """
+
+    successful_inches_keys = df.query('_out_crystalscore == 4')['_rxn_organic-inchikey'].unique()
+
+    df = (df[df['_rxn_organic-inchikey'].isin(successful_inches_keys)])
+
+    # If it is desired to make a non sense dataset
+    if data_preparation["shuffle_enabled"]:
         df = shuffle(df)
-    elif data_preparation["deep_shuffle_enabled"]: 
+    elif data_preparation["deep_shuffle_enabled"]:
         df = deep_shuffle(df)
 
     # Rename from raw to rxn
@@ -62,21 +67,19 @@ def prepare_dataset(df, data_preparation):
     return df
 
 
-##-----------------------------esqueleto-------------##
-
 def process_dataset(df, parameters):
-    inchikeys_count = df['_rxn_organic-inchikey'].value_counts()
+    # inches_key_count = df['_rxn_organic-inchikey'].value_counts()
 
-    # binarize class
+    # binary class
     crystal_score = df['_out_crystalscore']
     crystal_score = (crystal_score == 4).astype(int)
 
-    extr = parameters["extrpl"]
-    intrpl = parameters["intrpl"]
+    # extr = parameters["extrpl"]
+    interpolate = parameters["intrpl"]
 
     results = {
         'dataset_index': [],
-        'cv':[],    
+        'cv':[],
     #    'matrix':[],
         'precision_positive':[],
         'recall_positive':[],
@@ -87,17 +90,17 @@ def process_dataset(df, parameters):
     }
 
     # for each dataset, train and predict considering parameters
-    for dataset_name in parameters["dataset"]: 
+    for dataset_name in parameters["dataset"]:
 
-        selected_data = {    
-            'solV-chem': train.filter_data_for_solV(df),
-            'solUD-chem': train.filter_data_for_solUD(df)
+        selected_data = {
+            'solV-chem': train.filter_data_for_sol_v(df),
+            'solUD-chem': train.filter_data_for_sol_ud(df)
         }[dataset_name]
 
-        if(intrpl): train.std_train_test(selected_data, parameters["model"], crystal_score, dataset_name, results)
-       
+        if interpolate: train.std_train_test(selected_data, parameters["model"], crystal_score, dataset_name, results)
+
     # save results 
-    df =pd.DataFrame.from_dict(results, orient='columns')
+    df = pd.DataFrame.from_dict(results, orient='columns')
     df.to_csv('test3.csv')
 
     '''
@@ -110,4 +113,3 @@ def process_dataset(df, parameters):
 
 
     '''
- 
