82639f4aa7ce640392e7251613002b5a70425b84
Belu Ticona <ticona.belu@gmail.com>
2021-04-03 20:18:31 -0300

    [results] Added make summary

--- run.py
+++ run.py
@@ -1,8 +1,7 @@
-from src.data import process
+from src.data import pre_process
+from src.data import post_process
 from src.config import data_path
 from src.config import data_types_path
-from src.config import results_path
-from src.config import file_name_for
 import pandas as pd
 import yaml as yl
 import json
@@ -18,11 +17,13 @@ with open('parameters.yaml') as file, open(data_types_path) as json_file:
     df = pd.read_csv(data_path, header=0, dtype=dtypes)
 
     # Select reactions where the solvent produced at least a crystal score of 4
-    df = process.prepare_full_dataset(df, parameters["data_preparation"])
+    df = pre_process.prepare_full_dataset(df, parameters["data_preparation"])
 
-    results = process.process_dataset(df, parameters)
+    results = pre_process.process_dataset(df, parameters)
 
-    results.to_csv(results_path + file_name_for(parameters)[:-1] + '.csv')
+    post_process.save_and_summary(results, parameters)
+
+
     '''
     - generate results
         * from yaml
---
+++ src/data/post_process.py
@@ -0,0 +1,21 @@
+from src.config import file_name_for
+from src.config import results_path
+
+
+def save_and_summary(results, parameters):
+    full_results_file_name = file_name_for(parameters)[:-1] + '.csv'
+    summary_results_file_name = 'summary_' + full_results_file_name
+
+    results.to_csv(results_path + full_results_file_name, index=False)
+    results.drop(['cv', 'support_negative', 'support_positive'], axis=1, inplace=True)
+
+    std = results.groupby('dataset_index').std().add_suffix('_std')
+    min_results = results.groupby('dataset_index').min().add_suffix('_min')
+    max_results = results.groupby('dataset_index').max().add_suffix('_max')
+    summary = results.groupby('dataset_index').mean().add_suffix('_mean')
+
+    summary = summary.join(std).join(min_results).join(max_results)
+
+    summary.to_csv(summary_results_file_name)
+
+
