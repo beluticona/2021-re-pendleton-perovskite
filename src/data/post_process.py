88475c3628d13c4d77b4c0df2ccc7710ac8a82ab
Belu Ticona <ticona.belu@gmail.com>
2021-04-23 09:54:46 -0300

    [FT] Create diff output folder according to args

--- src/data/post_process.py
+++ src/data/post_process.py
@@ -1,39 +1,41 @@
 from src.config import file_name_for
 from src.config import results_path
 from src.config import set_results_folder
+from src.constants import GBC, KNN, FEAT_NAMES_IMPORTANCE, FEAT_VALUES_IMPORTANCE, STD_CV, LOO
 import pandas as pd
 import collections
-from src import constants
 
 
 def save_results(full_results, parameters):
-    full_results_descriptions = file_name_for(parameters)[:-1] + '.csv'
-    full_results_path = results_path + folder_for(parameters)
-    set_results_folder(full_results_path)
+    full_results_folder_path = results_path + folder_for(parameters)
+    set_results_folder(full_results_folder_path)
 
-    if parameters['model']['method'] == constants.GBC:
-        # TO REFACTOR
-        save_feature_importance(full_results_descriptions, full_results, constants.FEAT_VALUES_IMPORTANCE)
-        save_feature_importance(full_results_descriptions, full_results, constants.FEAT_NAMES_IMPORTANCE)
+    if parameters['model']['method'] == GBC:
+        pass
+        # @TODO: REFACTOR
+        #save_feature_importance(full_results_description, full_results, FEAT_VALUES_IMPORTANCE)
+        #save_feature_importance(full_results_description, full_results, FEAT_NAMES_IMPORTANCE)
     if parameters['intrpl']:
-        save_full_and_summary_for(full_results['std'], full_results_path, 'StandardTestTrain_Full.csv', 'StandardTestTrain_Summary.csv', parameters)
-    if parameters['exptrpl']:
-        save_full_and_summary_for(full_results['loo'], full_results_path, 'LeaveOneOut_Full.csv', 'LeaveOneOut_Summary.csv', parameters)
+        save_full_and_summary_for(full_results['std'], full_results_folder_path, 'StandardTestTrain_Full.csv', 'StandardTestTrain_Summary.csv', STD_CV)
+    if parameters['extrpl']:
+        save_full_and_summary_for(full_results['loo'], full_results_folder_path, 'LeaveOneOut_Full.csv', 'LeaveOneOut_Summary.csv', LOO)
 
-    f = open(results_path + 'info.txt', "a")
-    f.write(full_results_descriptions)
+    full_results_description = file_name_for(parameters)[:-1] + '.csv'
+    f = open(full_results_folder_path + 'info.txt', "a")
+    f.write(full_results_description)
     f.close()
 
 
-def save_full_and_summary_for(dict_results, full_results_path, full_result_filename, summary_filename, parameters):
+def save_full_and_summary_for(dict_results, full_results_path, full_result_filename, summary_filename, validation):
     df_results = pd.DataFrame.from_dict(dict_results, orient='columns')
-    df_results.to_csv(full_results_path + full_result_filename)
-    filter_only_metrics_columns(parameters, df_results)
+    df_results.to_csv(full_results_path + full_result_filename, index=None)
 
-    std = df_results.groupby('data_index').std().add_suffix('_std')
-    min_results = df_results.groupby('data_index').min().add_suffix('_min')
-    max_results = df_results.groupby('data_index').max().add_suffix('_max')
-    summary = df_results.groupby('data_index').mean().add_suffix('_mean')
+    summarize_results(df_results, validation)
+
+    std = df_results.groupby('data_index').std().add_suffix(' std')
+    min_results = df_results.groupby('data_index').min().add_suffix(' min')
+    max_results = df_results.groupby('data_index').max().add_suffix(' max')
+    summary = df_results.groupby('data_index').mean().add_suffix(' mean')
 
     summary = summary.join(std).join(min_results).join(max_results)
 
@@ -53,17 +55,21 @@ def save_feature_importance(full_results_file_name, results, feature):
     results.pop(feature, None)
 
 
-def filter_only_metrics_columns(parameters, results_df):
-    if parameters['extrpl']:
-        results_df.drop(['cv', 'chemical-name', 'inchi', 'support_negative', 'support_positive'], axis=1, inplace=True)
-    else:
-        results_df.drop(['cv', 'support_negative', 'support_positive'], axis=1, inplace=True)
+def summarize_results(results_df, validation):
+    # to delete for both cases: STD_CV and LOO
+    columns_name_to_delete = ['cv', 'support_negative', 'support_positive']
+    if validation == LOO:
+        columns_name_to_delete += ['chemical-name', 'inchi']
+
+    results_df.drop(columns_name_to_delete, axis=1, inplace=True)
 
 
 def folder_for(parameters):
     folder = ''
-    if parameters['model']['method'] == constants.KNN:
+    if parameters['model']['method'] == KNN:
         folder += 'knn'
+        if not parameters['model']['hyperparam-opt']:
+            folder += '1'
     norm = sum(value for value in parameters['model']['norm'].values())
     std = sum(value for value in parameters['model']['std'].values())
     if std > 0:
