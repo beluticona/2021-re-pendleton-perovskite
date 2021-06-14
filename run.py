c18bc6de04f6575236ed0476fe53d912827f5cf7
pacasi <ticona.belu@gmail.com>
2021-06-14 13:33:29 -0300

    split data upload

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 from src.data import pre_process
 from src.data import post_process
-from src.config import data_path, parameters_path, data_types_path
+from src.config import training_dataset_path, parameters_path, data_types_path
 from src.models import utils as model_utils
 import pandas as pd
 import yaml as yl
@@ -13,16 +13,17 @@ with open(parameters_path) as file, open(data_types_path) as json_file:
 
     dtypes = json.load(json_file)
     
-    df = pd.read_csv(data_path, header=0, dtype=dtypes)
+    df = pd.read_csv(training_dataset_path, header=0, dtype=dtypes)
 
     # Select reactions where the solvent produced at least a crystal score of 4
     df = pre_process.prepare_full_dataset(df, parameters["data_preparation"])
 
     #To analize metrics vs amount of data
-    sampling_fractions = [round(0.1*n,2) for n in range(2,11)]
-
+    #sampling_fractions = [round(0.1*n,2) for n in range(2,11)]
+    sampling_fractions = [1]
     full_results = model_utils.create_results_container(parameters)
 
+    # Set seed each run
     random_seeds = [random_seed for random_seed in range(parameters['runs'])]
 
     for seed in random_seeds:
--- src/config.py
+++ src/config.py
@@ -2,14 +2,20 @@
 
 from pathlib import Path
 
+# Paths to used files
 data_dir = Path('./')
-data_path = data_dir / 'data/raw/full-perovskite-data.csv' 
+raw_data_path = data_dir / 'data/raw/full-perovskite-data.csv'
+training_dataset_path = data_dir / 'data/training-perovskite-data.csv'
+testing_dataset_path = data_dir / 'data/testing-perovskite-data.csv'
 data_types_path = data_dir / 'data/raw/types.json'
 parameters_path = data_dir / 'parameters.yaml'
-results_path = data_dir / 'results/alternative/cant_exp_seed/'
 notebook_path = data_dir /'notebooks/results/'
+chemical_inventory_path = data_dir / 'data/metadata/chemical_inventory.csv'
 
+# Result path
+results_path = data_dir / 'results/alternative/importance/10'
 
+# Generate filename for results according to parameters
 def file_name_for(parameters):
     file_name = ''
     for setting in parameters:
